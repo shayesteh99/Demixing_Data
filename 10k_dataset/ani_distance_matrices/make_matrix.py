7db9dc5f0bc6f572c943c88eeebbf19cb917d3ac
bo1929 <aliosmanberk@sabanciuniv.edu>
2025-01-17 14:57:09 -0800

    Added trees with re-estimated branch lenghts, and distance matrices.

---
+++ 10k_dataset/ani_distance_matrices/make_matrix.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+import numpy as np
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(sys.argv[1], sep="\t", header=None)
+    df[2] = -3/4*np.log(1 - 4/3*df[2])
+    df.pivot(index=0, columns=1, values=2).to_csv(sys.argv[2], sep="\t")
+
